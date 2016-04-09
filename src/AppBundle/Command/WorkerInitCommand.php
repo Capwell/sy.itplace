872aacd49ff11f405190f5963a1eaf80add33cb9
Vgitare <Vgitare@yandex.ru>
2016-04-09 13:26:43 +0400

    Add WorkerInit

---
+++ src/AppBundle/Command/WorkerInitCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class WorkerInitCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('worker-init')
+            ->setDescription('...')
+            ->addArgument('argument', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+
+
+        $argument = $input->getArgument('argument');
+
+        if ($input->getOption('option')) {
+            // ...
+        }
+
+        $output->writeln('Command result.');
+    }
+
+}
